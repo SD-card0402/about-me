54a412311ab4de40c333227c1ff7dd21d57aae82
中澤めぐみ <ktc26a31e0006@edu.kyoto-tech.ac.jp>
2026-06-13 12:11:04 +0900

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -18,6 +18,15 @@ const Home = () => {
           <h3>私の素晴らしいサイト</h3>
         </div>
       </section>
+
+      <section id='navigation'>
+        <NavButton />
+      </section>
+
+      <section id='navigete'>
+        <h3>ボタンをクリックするとページが変わります</h3>
+      </section>
+
       <section id="center">
         <div className="hero">
           {<img src={iconpng} className='base' width='340' height='170' alt='アイコン画像' />
@@ -30,12 +39,6 @@ const Home = () => {
           <p>
             このサイトはReactによって構成されています
           </p>
-          <section id='navigation'>
-            <NavButton />
-          </section>
-          <section id='navigete'>
-            <h3>上のボタンをクリックするとページが変わります</h3>
-          </section>
         </div>
       </section>
 
